1b051f209f313ce31db084d15c28c4187365d6d2
Jaime A <jaimeangel1097@gmail.com>
2020-08-30 23:16:28 -0500

    Bug solucionado/

--- JS/app.js
+++ JS/app.js
@@ -4,10 +4,8 @@ const conf_k_on = "wIqeb9EIG0bs8oh1pZCWhOdzZjI2BfIM"
 //Variables
 let searchValue = document.querySelector(".search_bar")
 let valueSearch = document.querySelector(".search_input")
+let ctnGyphy = document.querySelector(".gifBox")
 
-
-
-let example = document.getElementById("boxExample")
 
 //Eventos
 searchValue.addEventListener("click",getSearch)
@@ -24,31 +22,33 @@ function getSearch(e){
 }
 
 async function getGyphy(valueBusqueda){
-    let url=`https://api.giphy.com/v1/gifs/search?api_key=${conf_k_on}&q=${valueBusqueda}&limit=1`
+    let url=`https://api.giphy.com/v1/gifs/search?api_key=${conf_k_on}&q=${valueBusqueda}&limit=12`
 
     const response = await fetch(url)
     const responseJSON = await response.json()
 
     try{
         let giphyResponse = responseJSON.data
+
         for(let i=0; i < giphyResponse.length; i++){
 
-            let img = giphyResponse[i].url 
-            let user = giphyResponse[i].user 
-            let title = giphyResponse[i].title
-
-
             console.log(giphyResponse[i])
+
+            let img = giphyResponse[i].images.original.url
+            let id = giphyResponse[i].id
         
-                      
-            let ctnExample = document.createElement("div")
-            ctnExample.classList.add("ctn-box")
-            let imgCtn = document.createElement("img")
-            imgCtn.textContent="src=${img}"
+            let divCtn = document.createElement("div")
+            divCtn.classList.add("box")
+
+            let imgCtn = document.createElement("div")
+            imgCtn.classList.add("box_img")
+
+            let imgGyphy = document.createElement("img")
+            imgGyphy.src=`${img}`
             
-            imgCtn.appendChild(ctnExample)
-            
-            example.appendChild(ctnExample)  
+            imgCtn.appendChild(imgGyphy)
+              divCtn.appendChild(imgCtn)
+                ctnGyphy.appendChild(divCtn)
             
         } 
     }catch{
