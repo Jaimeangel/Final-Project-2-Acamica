f14ab5760cc9a2794dd0441947f46ab16ea6da65
Jaime A <jaimeangel1097@gmail.com>
2020-12-01 10:11:26 -0500

    cam working right

--- JS/cam.js
+++ JS/cam.js
@@ -1,3 +1,6 @@
+const conf_k_on = "wIqeb9EIG0bs8oh1pZCWhOdzZjI2BfIM"
+const URL = "https://upload.giphy.com/v1/gifs"
+
 let btnCam = document.getElementById("btnCam")
 
 let introSlide = document.getElementById("introSlide")
@@ -9,8 +12,7 @@ let videoGum = document.getElementById("gum")
 let btnOne = document.getElementById("btn_1")
 let btnTwo = document.getElementById("btn_2")
 
-let mediaRecorder
-let recordedBlobs
+let recorder
 
 
 btnCam.addEventListener("click",(e)=>{
@@ -39,28 +41,154 @@ function onCam(){
 
   btnStyles()
 
-  const constraints = {
+ /*  const constraints = {
     audio: false,
     video: {
       width: 460,
       height: 300
     }
   } 
-  init(constraints) 
+  init(constraints)  */
+  function getStreamAndDisplay() {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          height: { max: 300 },
+        },
+      })
+      .then((stream) => {
+        const gumVideo = document.querySelector('video#gum')
+        gumVideo.srcObject = stream
+        gumVideo.play();
+
+        let btnStyles = function changedStyles(){
+    
+          //Style btn
+          btnOne.classList.remove("hoverBtnCamAct")
+          btnTwo.classList.add("hoverBtnCamAct")
+          
+          camSlideText.style.display="none"
+          videoGum.style.display="flex"
+          btnCam.style.display="block" 
+      
+          //Changed text btn
+          let btnOldCam = btnCam.childNodes[1]
+          console.log(btnOldCam)
+          let btnNewCam = document.createElement("button")
+          let btnNewCamText = document.createElement("p")
+          btnNewCamText.innerText = `GRABAR`
+      
+          btnNewCam.appendChild(btnNewCamText)
+          btnCam.replaceChild(btnNewCam,btnOldCam)  
+        }
+        btnStyles()
+
+        recorder = RecordRTC(stream, {
+          type: "gif",
+          frameRate: 1,
+          quality: 10,
+          width: 360,
+          height: 240,
+          onGifRecordingStarted() {
+            console.log('Grabando ahora mismo')
+          },
+        });
+      });
+  }
+  getStreamAndDisplay()
+}
+
+function recordCam(){
+  recorder.startRecording()
+
+  let btnStyles = function changedStyles(){
+    let btnOldCam = btnCam.childNodes[1]
+    console.log(btnOldCam)
+    let btnNewCam = document.createElement("button")
+    let btnNewCamText = document.createElement("p")
+    btnNewCamText.innerText = `FINALIZAR`
+
+    btnNewCam.appendChild(btnNewCamText)
+    btnCam.replaceChild(btnNewCam,btnOldCam) 
+  }
+  btnStyles()
+}
+
+function stopCam(){
+
+  let btnStyles = function changedStyles(){
+    let btnOldCam = btnCam.childNodes[1]
+    console.log(btnOldCam)
+  
+    let btnNewCam = document.createElement("button")
+    let btnNewCamText = document.createElement("p")
+    btnNewCamText.innerText = `SUBIR GIFO`
+  
+    btnNewCam.appendChild(btnNewCamText)
+    btnCam.replaceChild(btnNewCam,btnOldCam)
+  
+    console.log("para de grabar")
+  }
+  btnStyles()
+
+  recorder.stopRecording((recording) => {
+  console.log("grabacion:", recording);
+
+  const form = new FormData();
+  form.append("file", recorder.getBlob(), "myGif.gif");
+
+  btnCam.addEventListener("click",async (e)=>{
+    e.preventDefault()
+    if(btnCam.innerText===`SUBIR GIFO`){
+      try{
+        await fetch(`${URL}?api_key=${conf_k_on}`, { method: "POST", body: form });
+        console.log('Gif subido con exito')
+      }catch(error) {
+        console.log("algo salio mal: ", error);
+      } 
+    }
+
+  })
+  
+  });  
+
 }
 
 
-async function init(constraints){
+/* async function uploadGifApi(form){
+  if(e.target.classList.contains("btnStart")){
+    console.log('El gif se esta subiendo')
+      try {
+        await fetch(`${URL}?api_key=${conf_k_on}`, { method: "POST", body: form });
+      }catch(error) {
+        console.log("algo salio mal: ", error);
+      } 
+  }
+}  */
+
+
+
+
+
+
+
+
+
+
+
+
+/* async function init(constraints){
   try{
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     handleSuccess(stream);
   }catch(e){
-    console.error('navigator.getUserMedia error:', e);
+    console.error('No fue posible acceder a la camara', e);
   }
-}
+} */
 
 
-function handleSuccess(stream) {
+/* function handleSuccess(stream) {
   
   let btnStyles = function changedStyles(){
     
@@ -84,15 +212,18 @@ function handleSuccess(stream) {
   }
   btnStyles()
 
-  console.log('getUserMedia() got stream:', stream)
+  console.log('Se muestra en pantalla el strem', stream)
   window.stream = stream
 
   const gumVideo = document.querySelector('video#gum')
   gumVideo.srcObject = stream
-}
+} */
 
 
-function recordCam(e){
+
+
+
+/* function recordCam(e){
   startRecording()
 
   let btnOldCam = btnCam.childNodes[1]
@@ -106,8 +237,8 @@ function recordCam(e){
 
   console.log("Empieza grabando")
 }
-
-function stopCam(e){
+ */
+/* function stopCam(e){
   stopRecording();
 
   let btnOldCam = btnCam.childNodes[1]
@@ -122,12 +253,12 @@ function stopCam(e){
   btnCam.replaceChild(btnNewCam,btnOldCam)
 
   console.log("para de grabar")
-}
+} */
 
 
 
 
-function startRecording() {
+/* function startRecording() {
   recordedBlobs = []
   let options = {mimeType: 'video/webm;codecs=vp9,opus'}
 
@@ -141,16 +272,16 @@ function startRecording() {
 
   mediaRecorder.ondataavailable = handleDataAvailable
   mediaRecorder.start()
-}
+} */
 
 
-function stopRecording() {
+/* function stopRecording() {
   mediaRecorder.stop();
-}
+} */
 
-function handleDataAvailable(event) {
+/* function handleDataAvailable(event) {
   console.log('handleDataAvailable', event)
   if (event.data && event.data.size > 0) {
     recordedBlobs.push(event.data)
   }
-}
+} */
--- JS/paginacion.js
+++ JS/paginacion.js
@@ -1,21 +1,21 @@
 
-const galleryItems = document.querySelector(".gifBox").children;
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-const maxItem = 12;
-let index = 1;
-  
-const pagination = Math.ceil(galleryItems.length/maxItem);
+const galleryItems = document.querySelector(".gifBox").children
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+const maxItem = 12
+let index = 1
+
+const pagination = Math.ceil(galleryItems.length/maxItem)
 
 prev.addEventListener("click",function(){
-    index--;
-    check();
-    showItems();
+    index--
+    check(); 
+    showItems()
 })
 next.addEventListener("click",function(){
-  	index++;
-  	check();
-    showItems();  
+  	index++
+  	check(); 
+    showItems() 
 })
 
 function check(){
@@ -30,24 +30,38 @@ function check(){
     }else{
   	    prev.classList.remove("disabled");	
     }
-}
+} 
+
+function verifierGifLength(){
+
+	let promesa =  new Promise(function(resolve,reject){
+		
+		for(let i=0;i<galleryItems.length;i++){
+
+			if(galleryItems.length > 12){
+				resolve()
+			}else{
+				reject(error)
+			}
+		} 
+	})
+}  
 
 function showItems() {
   	for(let i=0;i<galleryItems.length; i++){
-  	 	galleryItems[i].classList.remove("show");
-  	 	galleryItems[i].classList.add("hide");
-
+  	 	galleryItems[i].classList.remove("show")
+  	 	galleryItems[i].classList.add("hide")
 
   	    if(i>=(index*maxItem)-maxItem && i<index*maxItem){
   	 	    // if i greater than and equal to (index*maxItem)-maxItem;
   		    // means  (1*8)-8=0 if index=2 then (2*8)-8=8
-            galleryItems[i].classList.remove("hide");
-            galleryItems[i].classList.add("show");
+            galleryItems[i].classList.remove("hide")
+            galleryItems[i].classList.add("show")
     	}
   	}
 }
 
-  window.onload=function(){
-  	showItems();
-  	check();
-  }
+window.onload=function(){
+	showItems()
+  	check(); 
+}
