aad237d0eb8a182255b3be3904734fa08524dc3b
Jaime A <jaimeangel1097@gmail.com>
2021-11-03 18:19:20 -0500

    img not found is add when user search

--- JS/apiCalls.js
+++ JS/apiCalls.js
@@ -56,131 +56,136 @@ function getMoreGif(e){
 }
 
 //get Gif from APIGiphy [Endpoint search]
-async function getGyphy(valueBusqueda){
+let getGyphy= async (valueBusqueda) => {
     
 let url=`https://api.giphy.com/v1/gifs/search?api_key=${conf_k_on}&q=${valueBusqueda}&limit=${numberGifInit}&offset=${offSetVariable}`
     const response = await fetch(url)
     const responseJSON = await response.json()
+    const gifData = responseJSON.data
+    console.log(gifData)
+    console.log(typeof(gifData))
 
     try{
         setTimeout(()=>{
-            let giphyResponse = responseJSON.data
-            for(let i=0; i < giphyResponse.length; i++){
-    
-                /* console.log(giphyResponse[i]) */
+            if(Object.keys(gifData).length){
+                gifData.forEach( Gif => {
+                    
+                    /* console.log(giphyResponse[i]) */
+            
+                    let img = Gif.images.original.url
+                    let title = Gif.title
+                    let user = Gif.username  
+                    let id = Gif.id
+                    
+                    //Creando Contenedor/incluye gif
+                    let divCtn = document.createElement("div")
+                    divCtn.style.backgroundImage = `url(${img})`
+                    divCtn.classList.add("box")
         
-                let img = giphyResponse[i].images.original.url
-                let title = giphyResponse[i].title
-                let user = giphyResponse[i].username  
-                let id = giphyResponse[i].id
-                
-                //Creando Contenedor/incluye gif
-                let divCtn = document.createElement("div")
-                divCtn.style.backgroundImage = `url(${img})`
-                divCtn.classList.add("box")
+                    //Creamos caja de botones
+                    let divBtn = document.createElement("div")
+                    divBtn.classList.add("box_li")
+        
+                    //Botones Individuales
+        
+                    let divHeart= document.createElement("div")
+                    divHeart.classList.add("box_li_btn_heart")
+                    let btnHeart = document.createElement("button")
+                    let imgBtnHeart= document.createElement("img")
+                    imgBtnHeart.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-fav-hover.svg")
+                    divHeart.setAttribute("data-id",`${id}`)
+                    btnHeart.appendChild(imgBtnHeart)
+                    divHeart.appendChild(btnHeart)
+                    divBtn.appendChild(divHeart)
+        
+                    //Event Handler Modal
+                    divHeart.addEventListener("click", ()=>{
+        
+                        imgGifUrl = img
+                        userGif = user
+                        nameGif = title
+                        idGif = id
+                        
+                        
     
-                //Creamos caja de botones
-                let divBtn = document.createElement("div")
-                divBtn.classList.add("box_li")
-    
-                //Botones Individuales
-    
-                let divHeart= document.createElement("div")
-                divHeart.classList.add("box_li_btn_heart")
-                let btnHeart = document.createElement("button")
-                let imgBtnHeart= document.createElement("img")
-                imgBtnHeart.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-fav-hover.svg")
-                divHeart.setAttribute("data-id",`${id}`)
-                btnHeart.appendChild(imgBtnHeart)
-                divHeart.appendChild(btnHeart)
-                divBtn.appendChild(divHeart)
-    
-                //Event Handler Modal
-                divHeart.addEventListener("click", ()=>{
-    
-                    imgGifUrl = img
-                    userGif = user
-                    nameGif = title
-                    idGif = id
+                        let favoriteGif = ()=> getGifFavorites(imgGifUrl,userGif,nameGif,idGif)
+                        favoriteGif()
+                        let deleteNotFound = ()=> nullGif()
+                        deleteNotFound() 
+                        let btnStyles = ()=>imgBtnHeart.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-fav-active.svg")
+                        btnStyles()
+                    }) 
+        
+                    let divDown= document.createElement("div")
+                    divDown.classList.add("box_li_btn_down")
+                    let btnDown = document.createElement("button")
+                    let imgBtnDown = document.createElement("img")
+                    imgBtnDown.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-download.svg")
+                    divDown.setAttribute("data-id",`${id}`)
+                    btnDown.appendChild(imgBtnDown)
+                    divDown.appendChild(btnDown)
+                    divBtn.appendChild(divDown)
+        
+        
+        
+                    let divMax= document.createElement("div")
+                    divMax.classList.add("box_li_btn_max")
+                    let btnMax = document.createElement("button")
+                    let imgBtnMax = document.createElement("img")
+                    imgBtnMax.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-max.svg")
+                    btnMax.appendChild(imgBtnMax)
+                    divMax.appendChild(btnMax)
+                    divBtn.appendChild(divMax)
+        
                     
+                    divCtn.appendChild(divBtn)
                     
-
-                    let favoriteGif = ()=> getGifFavorites(imgGifUrl,userGif,nameGif,idGif)
-                    favoriteGif()
-                    let deleteNotFound = ()=> nullGif()
-                    deleteNotFound() 
-                    let btnStyles = ()=>imgBtnHeart.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-fav-active.svg")
-                    btnStyles()
-                }) 
-    
-                let divDown= document.createElement("div")
-                divDown.classList.add("box_li_btn_down")
-                let btnDown = document.createElement("button")
-                let imgBtnDown = document.createElement("img")
-                imgBtnDown.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-download.svg")
-                divDown.setAttribute("data-id",`${id}`)
-                btnDown.appendChild(imgBtnDown)
-                divDown.appendChild(btnDown)
-                divBtn.appendChild(divDown)
-    
-    
-    
-                let divMax= document.createElement("div")
-                divMax.classList.add("box_li_btn_max")
-                let btnMax = document.createElement("button")
-                let imgBtnMax = document.createElement("img")
-                imgBtnMax.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-max.svg")
-                btnMax.appendChild(imgBtnMax)
-                divMax.appendChild(btnMax)
-                divBtn.appendChild(divMax)
-    
-                
-                divCtn.appendChild(divBtn)
-                
-                //Event Handler Modal
-                divMax.addEventListener("click", ()=>{
-                    imgGifUrl = img
-                    userGif = user
-                    nameGif = title
-                    idGif = id
-    
-                    buildingModal(imgGifUrl,userGif,nameGif,idGif)
-                })  
-    
-                let divAutor = document.createElement("div")
-                divAutor.classList.add("box_autor")
-    
-                let divPrfOne = document.createElement("div")
-                divPrfOne.classList.add("p_user") 
-                divPrfOne.innerText=`${user}`
-                divAutor.appendChild(divPrfOne)
-    
-                let divPrfTwo = document.createElement("div")
-                divPrfTwo.classList.add("p_name") 
-                divPrfTwo.innerText=`${title}`
-                divAutor.appendChild(divPrfTwo)
-    
-                divCtn.appendChild(divAutor) 
-    
-                ctnGyphy.appendChild(divCtn)
-    
-    
+                    //Event Handler Modal
+                    divMax.addEventListener("click", ()=>{
+                        imgGifUrl = img
+                        userGif = user
+                        nameGif = title
+                        idGif = id
+        
+                        buildingModal(imgGifUrl,userGif,nameGif,idGif)
+                    })  
+        
+                    let divAutor = document.createElement("div")
+                    divAutor.classList.add("box_autor")
+        
+                    let divPrfOne = document.createElement("div")
+                    divPrfOne.classList.add("p_user") 
+                    divPrfOne.innerText=`${user}`
+                    divAutor.appendChild(divPrfOne)
+        
+                    let divPrfTwo = document.createElement("div")
+                    divPrfTwo.classList.add("p_name") 
+                    divPrfTwo.innerText=`${title}`
+                    divAutor.appendChild(divPrfTwo)
+        
+                    divCtn.appendChild(divAutor) 
+        
+                    ctnGyphy.appendChild(divCtn)
+        
+                });
+                let btnStyles = function changedStyles(){
+                    if(contador > 1){
+                        prev.style.display="flex"
+                        next.style.display="flex"
+                    }
+                }
+                btnStyles()
+                paginationNumber()
+                contarGif()
+                contador++
+            }else{
+                notGifFound()
             }
+                
         },0)
     }catch{
         console.log("Este Gyphy no se encuentra")
     }
-
-    let btnStyles = function changedStyles(){
-        if(contador > 1){
-            prev.style.display="flex"
-            next.style.display="flex"
-        }
-    }
-    btnStyles()
-    paginationNumber()
-    contarGif()
-    contador++
 }
 
 
@@ -309,3 +314,31 @@ function paginationNumber(){
 	}
 }
 
+const notGifFound = () => {
+    ctnGyphy.style.display="none"
+    let divBox = document.createElement("div")
+    divBox.classList.add("nofFoundImg")
+    divBox.setAttribute("id","notFoundImgId")
+
+    let imgBox = document.createElement("img")
+    imgBox.setAttribute("src","GIFOS-UI-Desktop+Mobile 6/assets/icon-busqueda-sin-resultado.svg")
+    divBox.appendChild(imgBox)
+
+    let wordBox = document.createElement("div")
+    wordBox.classList.add("wordsFavorites")
+    let textBox1 = document.createElement("p")
+    textBox1.textContent = `Intenta con otra búsqueda.`
+    wordBox.appendChild(textBox1)
+    divBox.appendChild(wordBox)
+
+    let btnBox = document.createElement("button")
+    btnBox.setAttribute("id","btnNotFound")
+    btnBox.textContent="X"
+    btnBox.addEventListener("click",()=>{
+        ctnGyphy.style.display="grid"
+        divBox.remove()
+    })
+    divBox.appendChild(btnBox)
+
+    let nodeInsert = ctnMain.insertBefore(divBox,ctnGyphy)
+}
